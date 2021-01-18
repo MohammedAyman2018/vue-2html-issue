0d9a6cfbb79f73cfeeeb853da8610a6199482925
MohammedAyman2018 <emidobibo20@gmail.com>
2021-01-18 15:32:16 +0200

    reproduced

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vue-2html-bug-reproduce'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
